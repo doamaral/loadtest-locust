465a6799f652da2ffde6d4e2fb9692c063f92932
Lucas Amaral <lopesdoamaral@gmail.com>
2025-05-14 09:21:01 -0300

    Setup custom load shape

--- locustfiles/loscustfile.py
+++ locustfiles/loscustfile.py
@@ -11,3 +11,28 @@ class MessageLoadTester(HttpUser):
         }
         response = self.client.post("/log", json=body)
         print(response.status_code, response.text)
+
+class CustomLoadShape(LoadTestShape):
+    stages = [
+        {'duration': 5, 'users': 5, 'spawn_rate': 1},
+        {'duration': 10, 'users': 100, 'spawn_rate': 100},
+        {'duration': 40, 'users': 20, 'spawn_rate': 20},
+        {'duration': 60, 'users': 100, 'spawn_rate': 100},
+        {'duration': 70, 'users': 20, 'spawn_rate': 20},
+        {'duration': 90, 'users': 100, 'spawn_rate': 100},
+        {'duration': 100, 'users': 20, 'spawn_rate': 20},
+        {'duration': 120, 'users': 100, 'spawn_rate': 100},
+        {'duration': 130, 'users': 20, 'spawn_rate': 20},
+        {'duration': 150, 'users': 100, 'spawn_rate': 100}
+    ]
+
+    def tick(self):
+        run_time = self.get_run_time()
+        for i, stage in enumerate(self.stages):
+            if run_time < stage["duration"]:
+                print(
+                    f"🕒 Running Stage {i+1}: {stage['users']} users, {stage['spawn_rate']} spawn rate")
+                return (stage["users"], stage["spawn_rate"])
+        print("🏁 Test Complete - No More Stages")
+        return None
+
